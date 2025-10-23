61ff4e977673234d507aab3bf269eecc1fbb22ac
marceliomp <marcelio.aguiac@gmail.com>
2025-10-23 17:37:58 -0300

    Load Alvo branding from data URLs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ALVO_FAVICON_DATA_URL } from "./branding";
+
+function ensureFavicon() {
+  if (typeof document === "undefined") return;
+  let link = document.querySelector("link[rel='icon']");
+  if (!link) {
+    link = document.createElement("link");
+    link.rel = "icon";
+    document.head.appendChild(link);
+  }
+  link.type = "image/png";
+  link.href = ALVO_FAVICON_DATA_URL;
+}
+
+ensureFavicon();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
